e340d96f261058aefd10220a2f067639734305d8
KasparKoorits <kaspar.koorits@voco.ee>
2024-11-19 09:50:07 +0200

    Created article controller and route, refactored application code

--- index.js
+++ index.js
@@ -18,40 +18,15 @@ app.engine(
     layoutsDir: __dirname + "/views/layouts/",
   })
 );
-
-// setup static public dir
 app.use(express.static("public"));
-const mysql = require("mysql2");
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// create database connection
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "joga_mysql",
-});
+const articleRoutes = require("./routes/article");
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("connected to joga_mysql db");
-});
-
-// show page
-app.get("/", (req, res) => {
-  let query = "SELECT * FROM article";
-  let articles = [];
-  con.query(query, (err, result) => {
-    if (err) throw err;
-    articles = result;
-    console.log(articles);
-    res.render("index", {
-      title: "Homepage",
-      articles: articles,
-    });
-  });
-});
+app.use("/", articleRoutes);
+app.use("/article", articleRoutes);
 
 app.get("/author/:id", (req, res) => {
   let query = `SELECT * FROM article WHERE author_id = ?`;
@@ -69,27 +44,6 @@ app.get("/author/:id", (req, res) => {
         res.render("index", {
           title: author.name,
           articles: articles,
-        });
-      }
-    );
-  });
-});
-
-// show article by this slug
-app.get("/article/:slug", (req, res) => {
-  let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`;
-  con.query(query, (err, result) => {
-    if (err) throw err;
-    let article = result;
-    con.query(
-      `SELECT * FROM author WHERE id = ?`,
-      [article[0].author_id],
-      (err, aresult) => {
-        if (err) throw err;
-        let author = aresult[0];
-        article[0].author = author;
-        res.render("article", {
-          article: article,
         });
       }
     );
