6db77a43ee46e7226db9eff0d6fc5b3a5951e1af
KasparKoorits <kaspar.koorits@voco.ee>
2025-01-01 15:29:22 +0200

    Created author controller and route, refactored application code

--- index.js
+++ index.js
@@ -28,27 +28,7 @@ const articleRoutes = require("./routes/article");
 app.use("/", articleRoutes);
 app.use("/article", articleRoutes);
 
-app.get("/author/:id", (req, res) => {
-  let query = `SELECT * FROM article WHERE author_id = ?`;
-  let articles = [];
-  con.query(query, [req.params.id], (err, result) => {
-    if (err) throw err;
-    articles = result;
-    console.log(articles);
-    con.query(
-      `SELECT * FROM author WHERE id = ?`,
-      [req.params.id],
-      (err, aresult) => {
-        if (err) throw err;
-        let author = aresult[0];
-        res.render("index", {
-          title: author.name,
-          articles: articles,
-        });
-      }
-    );
-  });
-});
+app.use("/author, authorRoutes");
 
 // app starts here
 app.listen(3000, () => {
